543a83c063d141528650f45583bb1baef2e4ac36
PeanutKingSolutionOffical <142392722+PeanutKingSolutionOffical@users.noreply.github.com>
2024-07-04 10:23:23 +0800

    add documentation about component tests

--- test.ts
+++ test.ts
@@ -1,9 +1,11 @@
 // tests go here; this will not be compiled when this package is used as an extension.
 basic.forever(function () {
-    serial.writeNumber(pksdriver.compoundEyeRead(pksdriver.compoundEyeData.max_eye))
-    serial.writeNumber(pksdriver.compoundEyeRead(pksdriver.compoundEyeData.max_eye_value))
+    serial.writeLine('compound eye readings')
+    serial.writeNumber(pksdriver.compoundEyeRead(pksdriver.compoundEyeData.max_eye)) 
+    serial.writeNumber(pksdriver.compoundEyeRead(pksdriver.compoundEyeData.max_eye_value)) // if the test is passed, a non-zero number should be shown
     serial.writeString("----------")
-    serial.writeNumber(pksdriver.compass_get_yaw())
+    serial.writeString("compass reading")
+    serial.writeNumber(pksdriver.compass_get_yaw()) // if the test is passed, the number returned should change as the compass is rotated
     serial.writeString("------------")
     basic.pause(1000)
 })
