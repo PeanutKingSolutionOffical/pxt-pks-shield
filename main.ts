77f46df3a3a3fb6c18ad344cbb5ab38bbf3050fb
PeanutKingSolutionOffical <142392722+PeanutKingSolutionOffical@users.noreply.github.com>
2024-07-04 10:34:35 +0800

    follow JS naming conventions

--- main.ts
+++ main.ts
@@ -3,42 +3,42 @@
 //% icon="\uf2db" 
 //% block="PKS drivers"
 namespace pksdriver {
-    export enum compoundEyeData {
+    export enum COMPOUND_EYE_DATA {
         //% block="eye_1"
-        ir_1,
+        IR_1,
         //% block="eye_2"
-        ir_2,
+        IR_2,
         //% block="eye_3"
-        ir_3,
+        IR_3,
         //% block="eye_4"
-        ir_4,
+        IR_4,
         //% block="eye_5"
-        ir_5,
+        IR_5,
         //% block="eye_6"
-        ir_6,
+        IR_6,
         //% block="eye_7"
-        ir_7,
+        IR_7,
         //% block="eye_8"
-        ir_8,
+        IR_8,
         //% block="eye_9"
-        ir_9,
+        IR_9,
         //% block="eye_10"
-        ir_10,
+        IR_10,
         //% block="eye_11"
-        ir_11,
+        IR_11,
         //% block="eye_12"
-        ir_12,
-        //% block="max_eye_value"
+        IR_12,
+        //% block="MAX_EYE_VALUE"
         //% weight=99
-        max_eye_value,
-        //% block="max_eye"
+        MAX_EYE_VALUE,
+        //% block="MAX_EYE"
         //% weight=100
-        max_eye,
-        //% block="angle"
+        MAX_EYE,
+        //% block="ANGLE"
         //% weight=98
-        angle,
-        //% block="mode"
-        mode,
+        ANGLE,
+        //% block="MODE"
+        MODE,
     }
 
     /**
@@ -46,7 +46,7 @@ namespace pksdriver {
     */
     //% blockId=compoundEye block="compound eye $compound_eye_data"  subcategory="Robot"
     //% weight=50
-    export function compoundEyeRead(compound_eye_data: compoundEyeData): number {
+    export function compoundEyeRead(compound_eye_data: COMPOUND_EYE_DATA): number {
         pins.i2cWriteNumber(
             0x13,
             compound_eye_data,
@@ -56,9 +56,9 @@ namespace pksdriver {
         let temp = pins.i2cReadNumber(0x13, NumberFormat.UInt8LE, false);
         if (temp == 255) {
             return -1;
-        } else if (compound_eye_data == compoundEyeData.angle) {
+        } else if (compound_eye_data == COMPOUND_EYE_DATA.ANGLE) {
             temp *= 2;
-        } else if (compound_eye_data == compoundEyeData.max_eye) {
+        } else if (compound_eye_data == COMPOUND_EYE_DATA.MAX_EYE) {
             temp += 1;
         }
         return temp;
@@ -86,7 +86,7 @@ namespace pksdriver {
     */
     //% block="yaw (Unit: deg)" subcategory="Acceleration"
     //% weight=70
-    export function compass_get_yaw(): number {
+    export function compassGetYAW(): number {
         let yaw_ang = 0;
         pins.i2cWriteNumber(Compass.BOARD_ID, Compass.GET_YAW, NumberFormat.UInt8BE, false);
         let compass_raw = pins.i2cReadBuffer(Compass.BOARD_ID, 2, false);
--- test.ts
+++ test.ts
@@ -1,11 +1,11 @@
 // tests go here; this will not be compiled when this package is used as an extension.
 basic.forever(function () {
     serial.writeLine('compound eye readings')
-    serial.writeNumber(pksdriver.compoundEyeRead(pksdriver.compoundEyeData.max_eye)) 
-    serial.writeNumber(pksdriver.compoundEyeRead(pksdriver.compoundEyeData.max_eye_value)) // if the test is passed, a non-zero number should be shown
+    serial.writeNumber(pksdriver.compoundEyeRead(pksdriver.COMPOUND_EYE_DATA.MAX_EYE)) 
+    serial.writeNumber(pksdriver.compoundEyeRead(pksdriver.COMPOUND_EYE_DATA.MAX_EYE_VALUE)) // if the test is passed, a non-zero number should be shown
     serial.writeString("----------")
     serial.writeString("compass reading")
-    serial.writeNumber(pksdriver.compass_get_yaw()) // if the test is passed, the number returned should change as the compass is rotated
+    serial.writeNumber(pksdriver.compassGetYAW()) // if the test is passed, the number returned should change as the compass is rotated
     serial.writeString("------------")
     basic.pause(1000)
 })
